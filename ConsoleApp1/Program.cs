b92d4b51828eb2060240de0cce14ac9c3d9e64aa
rohi <richardRK@users.noreply.github.com>
2020-11-18 13:09:25 +0530

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+            var result = input.Reverse();
+
+
+            string ext = String.Format("[{0}]", string.Join(",", result.Select(n => "\"" + n + "\"")));
+
+
+            Console.WriteLine(ext);
+
+          
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
